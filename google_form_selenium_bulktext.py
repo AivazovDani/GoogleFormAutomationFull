732e8a7b9dd3d6a1fbb93ce9b558fe0298415d3d
Yordan Ayvazov <ayvazov.dani@gmail.com>
2026-04-14 14:08:44 +0300

    Update google_form_selenium_bulktext.py

--- google_form_selenium_bulktext.py
+++ google_form_selenium_bulktext.py
@@ -327,16 +327,17 @@ def process_single_record(record, other_fields, dropdown):
     chrome_options = Options()
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
-    chrome_options.add_argument('--disable-blink-features=AutomationControlled')
-    chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    chrome_options.add_experimental_option('useAutomationExtension', False)
     chrome_options.add_argument('--display=:99')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--disable-software-rasterizer')
+    chrome_options.add_argument('--disable-extensions')
+    chrome_options.add_argument('--remote-debugging-port=9222')
+    chrome_options.add_argument('--window-size=1920,1080')
 
 
 
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=chrome_options)
-    driver.maximize_window()
 
     try:
         # Open form
