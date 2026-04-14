34248ded2ec0ed774f936f0f221f4d4bb6895c52
Yordan Ayvazov <ayvazov.dani@gmail.com>
2026-04-14 14:07:29 +0300

    Update zarkon_brand_registration.py

--- zarkon_brand_registration.py
+++ zarkon_brand_registration.py
@@ -191,19 +191,16 @@ def process_single_record(record, other_fields):
     chrome_options = Options()
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
-    chrome_options.add_argument('--profile-directory=Default')
-    chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    chrome_options.add_experimental_option('useAutomationExtension', False)
-    chrome_options.add_argument('--lang=en-US')  # ADD THIS
-    chrome_options.add_experimental_option('prefs', {
-    'intl.accept_languages': 'en-US,en'
-    })
     chrome_options.add_argument('--display=:99')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--disable-software-rasterizer')
+    chrome_options.add_argument('--disable-extensions')
+    chrome_options.add_argument('--remote-debugging-port=9222')
+    chrome_options.add_argument('--window-size=1920,1080')
 
 
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=chrome_options)
-    driver.maximize_window()
 
     try:
         # Open form
