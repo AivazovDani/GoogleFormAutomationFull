759af50e67541cc39d086c53ad1fa480cf266ade
Yordan Ayvazov <ayvazov.dani@gmail.com>
2026-04-14 14:30:43 +0300

    Update google_form_selenium_tcrpy.py

--- google_form_selenium_tcrpy.py
+++ google_form_selenium_tcrpy.py
@@ -334,7 +334,7 @@ def process_single_record(record, other_fields, dropdown):
     chrome_options = Options()
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
-    chrome_options.add_argument('--display=:99')
+    chrome_options.add_argument('--headless=new')
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--disable-software-rasterizer')
     chrome_options.add_argument('--disable-extensions')
