9ee2dda20862f053767e14960ce334cd5443f259
Yordan Ayvazov <ayvazov.dani@gmail.com>
2026-04-14 14:07:53 +0300

    Update xeebi.py

--- xeebi.py
+++ xeebi.py
@@ -331,20 +331,17 @@ def process_single_record(record, other_fields, radio):
     chrome_options = Options()
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
-    chrome_options.add_argument('--disable-blink-features=AutomationControlled')
-    chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    chrome_options.add_experimental_option('useAutomationExtension', False)
-    chrome_options.add_argument('--lang=en-US')
-    chrome_options.add_experimental_option('prefs', {
-    'intl.accept_languages': 'en-US,en'
-    })
     chrome_options.add_argument('--display=:99')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--disable-software-rasterizer')
+    chrome_options.add_argument('--disable-extensions')
+    chrome_options.add_argument('--remote-debugging-port=9222')
+    chrome_options.add_argument('--window-size=1920,1080')
 
 
 
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=chrome_options)
-    driver.maximize_window()
 
     try:
         # Open form
